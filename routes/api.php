b08254841a305fb88df501655d336c04cbf89951
Lê Chí Huy <lechihuy1062001@gmail.com>
2020-11-19 16:08:28 +0700

    Change method routes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/login', 'AdminController@login');
+Route::post('/login', 'AdminController@login');
 Route::get('/logout', 'AdminController@logout');
 Route::get('/refresh', 'AdminController@refresh');
 Route::get('/me', 'AdminController@me');
